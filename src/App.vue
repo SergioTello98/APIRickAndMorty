157090f0fba625d2abff2bce01a9ffe8d82210a6
Sergio Tello <sergio@arrenda.mx>
2023-01-17 21:13:47 -0600

    Se realizo un ejemplo de consumir una api en vue y axios

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class="section">
+        <img class="is-flex-mobile img-rick" src="https://i.pinimg.com/originals/ca/f8/a9/caf8a9529d20f213eccdc6f3c611964f.png" alt="RickandMortyLogo">
+        <h1 class="title-rick">Personajes de Rick and Morty</h1>
+    </section>
+    <personajes></personajes>
+  </div>
+</template>
+
+<script>
+import Personajes from './components/Personajes.vue';
+  export default {
+    components: {
+      Personajes
+    }
+  }
+
+</script>
---
+++ src/components/Personajes.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <div class="columns is-multiline">
+    <div class="column is-4 $card-content-padding" v-for="(personaje, p) in personajes " key="p">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img :src="personaje.image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                  <img src="https://i.pinimg.com/originals/ca/f8/a9/caf8a9529d20f213eccdc6f3c611964f.png">
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">{{ personaje.name }}</p>
+              <p class="subtitle is-6">{{ personaje.status }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+export default{
+  beforeMount: function() {
+    let url="https://rickandmortyapi.com/api/character"
+    let currentObj = this;
+    axios(url).then(function(response){
+      console.log(response.data.results);
+      currentObj.personajes = response.data.results;
+    })
+  },
+  data: function(){
+    return {
+      personajes: []
+    }
+  }
+}
+
+</script>
